54757747d500bbfa655f17cc61ba170b678f11cd
Richard Shi <kmshi_array@yahoo.com>
2014-06-19 15:31:21 +0800

    Rename NgResource and NgRoute

--- ng/NgResource.hx
+++ ng/NgResource.hx
@@ -5,13 +5,15 @@ package ;
  * @author Richard Shi
  */
 @:initPackage
-class Resource
+class NgResource
 {
     private static function __init__() : Void untyped {
         #if embed_js
           haxe.macro.Compiler.includeFile("www/js/bower_components/angular-resource/angular-resource.min.js");
         #end
 		//add "ngResource" to global module dependencies
+		if (Angular.isUndefined(window.hxdeps))window.hxdeps = [];
+		window.hxdeps.push("ngResource");
     }
 	
 }
--- ng/NgRoute.hx
+++ ng/NgRoute.hx
@@ -5,13 +5,15 @@ package ;
  * @author Richard Shi
  */
 @:initPackage
-class Route
+class NgRoute
 {
     private static function __init__() : Void untyped {
         #if embed_js
           haxe.macro.Compiler.includeFile("www/js/bower_components/angular-route/angular-route.min.js");
         #end
 		//add "ngRoute" to global module dependencies
+		if (Angular.isUndefined(window.hxdeps))window.hxdeps = [];
+		window.hxdeps.push("ngRoute");
     }
 	
 }
