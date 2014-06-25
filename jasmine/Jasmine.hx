25f6b584e7dab3fca8b4a8b3e3d7fa7b7a263a9a
Richard Shi <kmshi_array@yahoo.com>
2014-06-25 17:36:42 +0800

    Add jasmine haxe extern from https://github.com/rjanicek/jasmine-haxe

---
+++ jasmine/Jasmine.hx
@@ -0,0 +1,24 @@
+package jasmine;
+import js.Lib;
+
+/**
+ * @link http://pivotal.github.com/jasmine/jsdoc/index.html
+ */
+
+class Jasmine {
+	public static function getEnv() : Env untyped {
+		return jasmine.getEnv();
+	}
+	
+	public static function newTrivialReporter() : Reporter {
+		return untyped __js__("new jasmine.TrivialReporter()");
+	}
+	
+	public static function newHtmlReporter() : Reporter {
+		return untyped __js__("new jasmine.HtmlReporter()");
+	}
+	
+	public static function createSpy( name : String ) : Spy untyped {
+		return jasmine.createSpy(name);
+	}
+}
